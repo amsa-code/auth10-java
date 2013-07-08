b5abec221c2c81b6caf80f0eeeca4f9006344844
Stephen O'Hair <stephen.ohair@gmail.com>
2013-07-08 14:48:56 +1000

    added token decryption

--- src/main/java/com/auth10/federation/FederatedLoginManager.java
+++ src/main/java/com/auth10/federation/FederatedLoginManager.java
@@ -41,86 +41,98 @@ import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.ISODateTimeFormat;
 
 public class FederatedLoginManager {
-	private static final DateTimeFormatter CHECKING_FORMAT = ISODateTimeFormat.dateTime().withZone(DateTimeZone.UTC);
-	
-	private HttpServletRequest request;
-	private FederatedAuthenticationListener listener;
+	private static final DateTimeFormatter CHECKING_FORMAT = ISODateTimeFormat
+			.dateTime().withZone(DateTimeZone.UTC);
+
+	private final HttpServletRequest request;
+	private final FederatedAuthenticationListener listener;
 
 	public static FederatedLoginManager fromRequest(HttpServletRequest request) {
 		return fromRequest(request, null);
 	}
 
-	public static FederatedLoginManager fromRequest(HttpServletRequest request, FederatedAuthenticationListener listener) {
+	public static FederatedLoginManager fromRequest(HttpServletRequest request,
+			FederatedAuthenticationListener listener) {
 		return new FederatedLoginManager(request, listener);
 	}
 
-	protected FederatedLoginManager(HttpServletRequest request, FederatedAuthenticationListener listener) {
+	protected FederatedLoginManager(HttpServletRequest request,
+			FederatedAuthenticationListener listener) {
 		this.request = request;
 		this.listener = listener;
 	}
 
-	public final FederatedPrincipal authenticate(String token, HttpServletResponse response) throws FederationException {
+	public final FederatedPrincipal authenticate(String token,
+			HttpServletResponse response) throws FederationException {
 		List<Claim> claims = null;
 
 		try {
 			SamlTokenValidator validator = new SamlTokenValidator();
 
 			this.setTrustedIssuers(validator);
-			
+
 			this.setAudienceUris(validator);
 
-			this.setThumbprint(validator);			
+			this.setThumbprint(validator);
 
 			claims = validator.validate(token);
 
 			FederatedPrincipal principal = new FederatedPrincipal(claims);
-			
-			if (listener != null) listener.OnAuthenticationSucceed(principal);
-			
-			return principal;			
+
+			if (listener != null)
+				listener.OnAuthenticationSucceed(principal);
+
+			return principal;
 		} catch (FederationException e) {
 			throw e;
 		} catch (Exception e) {
 			throw new FederationException("Federated Login failed!", e);
-		} finally {
-			if (claims == null) {
-				request.getSession().invalidate();
-				throw new FederationException("Invalid Token");
-			}
 		}
+		// finally {
+		// if (claims == null) {
+		// request.getSession().invalidate();
+		// throw new FederationException("Invalid Token");
+		// }
+		// }
 	}
-		
-	protected void setTrustedIssuers(SamlTokenValidator validator) 
+
+	protected void setTrustedIssuers(SamlTokenValidator validator)
 			throws FederationException {
-		String[] trustedIssuers = FederatedConfiguration.getInstance().getTrustedIssuers();
+		String[] trustedIssuers = FederatedConfiguration.getInstance()
+				.getTrustedIssuers();
 		if (trustedIssuers != null) {
 			validator.getTrustedIssuers().addAll(Arrays.asList(trustedIssuers));
-		}		
+		}
 	}
-	
-	protected void setAudienceUris(SamlTokenValidator validator) 
+
+	protected void setAudienceUris(SamlTokenValidator validator)
 			throws FederationException {
-		String[] audienceUris = FederatedConfiguration.getInstance().getAudienceUris();
+		String[] audienceUris = FederatedConfiguration.getInstance()
+				.getAudienceUris();
 		for (String audienceUriStr : audienceUris) {
 			try {
 				validator.getAudienceUris().add(new URI(audienceUriStr));
 			} catch (URISyntaxException e) {
-				throw new FederationException("Federated Login Configuration failure: Invalid Audience URI", e);
+				throw new FederationException(
+						"Federated Login Configuration failure: Invalid Audience URI",
+						e);
 			}
 		}
 	}
-	
+
 	protected void setThumbprint(SamlTokenValidator validator)
 			throws FederationException {
-		String thumbprint = FederatedConfiguration.getInstance().getThumbprint();
+		String thumbprint = FederatedConfiguration.getInstance()
+				.getThumbprint();
 		validator.setThumbprint(thumbprint);
 	}
 
 	public static String getFederatedLoginUrl(String returnURL) {
-		return getFederatedLoginUrl(null, null, returnURL); 
+		return getFederatedLoginUrl(null, null, returnURL);
 	}
-	
-	public static String getFederatedLoginUrl(String realm, String replyURL, String returnURL) {
+
+	public static String getFederatedLoginUrl(String realm, String replyURL,
+			String returnURL) {
 		Calendar c = Calendar.getInstance();
 
 		String encodedDate = CHECKING_FORMAT.print(c.getTimeInMillis());
@@ -131,21 +143,26 @@ public class FederatedLoginManager {
 		String encodedRealm = URLUTF8Encoder.encode(realm);
 
 		String encodedReply = null;
-		if (replyURL != null) {			
+		if (replyURL != null) {
 			encodedReply = URLUTF8Encoder.encode(replyURL);
-		}
-		else {
-			encodedReply = (FederatedConfiguration.getInstance().getReply() != null) ? URLUTF8Encoder.encode(FederatedConfiguration.getInstance().getReply()) : null;
+		} else {
+			encodedReply = (FederatedConfiguration.getInstance().getReply() != null) ? URLUTF8Encoder
+					.encode(FederatedConfiguration.getInstance().getReply())
+					: null;
 		}
 
-		String encodedRequest = (returnURL != null) ? URLUTF8Encoder.encode(returnURL) : "";
+		String encodedRequest = (returnURL != null) ? URLUTF8Encoder
+				.encode(returnURL) : "";
 
 		String federatedLoginURL = FederatedConfiguration.getInstance()
 				.getStsUrl()
-				+ "?wa=wsignin1.0&wtrealm="	+ encodedRealm
-				+ "&wctx=" + encodedRequest
+				+ "?wa=wsignin1.0&wtrealm="
+				+ encodedRealm
+				+ "&wctx="
+				+ encodedRequest
 				+ "&id=passive"
-				+ "&wct=" + encodedDate;
+				+ "&wct="
+				+ encodedDate;
 
 		if (encodedReply != null) {
 			federatedLoginURL += "&wreply=" + encodedReply;
